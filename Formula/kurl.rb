78a14a968fc7fb63859607041482d98ea7c2891e
Kiran Sebastian <kiran@greyshepherd.com>
2025-10-23 22:02:21 +0800

    Update kurl v0.1.1

--- Formula/kurl.rb
+++ Formula/kurl.rb
@@ -1,8 +1,8 @@
 class Kurl < Formula
   desc "A lightweight CLI tool to parse and output URL components in plain text or JSON"
   homepage "https://github.com/kzbt/kurl"
-  url "https://github.com/kzbt/kurl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6a6d2d5299e8ab5406813dc5c4051018477834a8bd5b49e21e7fa00f11c69912"
+  url "https://github.com/kzbt/kurl/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "c007aff8e23afa75d146f35b92b7f72f8348b4f35903feb7a7e4a00dbaca8d7c"
   license "MIT"
 
   depends_on "rust" => :build
