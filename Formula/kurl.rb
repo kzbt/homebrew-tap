0f577a11e18543040886153689ea64f462a12fa4
Kiran Sebastian <2608311+kzbt@users.noreply.github.com>
2025-10-23 15:01:16 +0000

    kurl: add 0.1.1 bottle.

--- Formula/kurl.rb
+++ Formula/kurl.rb
@@ -5,6 +5,13 @@ class Kurl < Formula
   sha256 "c007aff8e23afa75d146f35b92b7f72f8348b4f35903feb7a7e4a00dbaca8d7c"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/kzbt/homebrew-tap/releases/download/kurl-0.1.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "bb6407ab5a7cb7fe33586f006c27550daa611ff3ee5c6fd9a5bc81fb5cd86119"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1b9ce792ffc148608e52f401c8b154fcec10c08be9b19731433ad2739fab09c7"
+  end
+
   depends_on "rust" => :build
 
   def install
